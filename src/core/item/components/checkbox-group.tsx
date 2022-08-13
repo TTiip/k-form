0034d3b83f3c065083cd731e50f85b1be74cf98d
TTiip <2711430287@qq.com>
2022-08-13 23:03:44 +0800

    feat: 添加 checkbox-group 多选框组件 & 添加组件选项禁用功能。

--- src/core/item/components/checkbox-group.tsx
+++ src/core/item/components/checkbox-group.tsx
@@ -14,6 +14,7 @@ export default defineComponent({
       <ElCheckboxGroup
         model-value = { form?.[options?.compSetting?.key] ?? [] }
         onChange = { fn?.onChange }
+        {...options.compSetting}
       >
         {
           options?.compSetting?.options.map((item: any) =>
