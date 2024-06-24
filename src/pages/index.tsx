42ffa5492104ebd6524764c656b8df2bccfe6096
TTiip <2711430287@qq.com>
2024-06-24 21:09:04 +0800

    feat: 表单类型功能(校验完善)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
-// import FormByTsx from './case/form-by-tsx'
-import FormByVue from './case/form-by-vue/index.vue'
+import FormByTsx from './case/form-by-tsx'
+// import FormByVue from './case/form-by-vue/index.vue'
 
 export default defineComponent({
   name: 'PlayGround',
   setup () {
     return () => (
       <div>
-        {/* <FormByTsx /> */}
-        <FormByVue />
+        <FormByTsx />
+        {/* <FormByVue /> */}
         <div i-carbon-campsite text-4xl inline-block />
       </div>
     )
